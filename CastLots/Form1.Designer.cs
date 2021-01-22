aa0119199008a3214b7c88fa8e77ca9da83edfbc
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-01-23 01:18:57 +0300

    Add project files.

---
+++ CastLots/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace CastLots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonStart.Location = new System.Drawing.Point(169, 37);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(225, 82);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelText
+            // 
+            this.labelText.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelText.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelText.Location = new System.Drawing.Point(33, 140);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(524, 90);
+            this.labelText.TabIndex = 1;
+            this.labelText.Text = ".";
+            this.labelText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 273);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CastLots Technology";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelText;
+    }
+}
+
---
+++ CastLots/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CastLots
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string techName = "";
+        int number;
+        string[] technoList = {"Html","Css","Javascript","ReactJs","VueJs","TypeScript","Boostrap","Jquery","Ajax","ElasticSearch","Redis","Docker","OOP","EntityFramework","Linq","Postgresql",".NetMVC"
+                ,"ConsoleApp","Web Servis,Api,WCF","Mikroservis Mimarisi","3.Party Component","MSSQL","TSQL","SAPCrystalReport","AWS",".Net Core","Rest Api","SOAP","JSON","XML","Protractor","Selenium","Apium"
+        };
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+            Random rndNumber = new Random();
+            number = rndNumber.Next(0, 33);
+            
+            for (int i = 0; i < technoList.Length; i++)
+            {
+                techName = technoList[number];
+                labelText.Text = techName;
+            }
+        }
+    }
+}
